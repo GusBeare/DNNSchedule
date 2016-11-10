68feb2c4939c162847e63c2d537c00fadab0fed1
Gus Beare <gus@carawaydesign.com>
2016-11-10 13:18:14 +0000

    Moved test web site into  same folder

---
+++ TestWebSite/Home.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DNNScheduledTask.DotNetNuke;
+
+
+namespace TestWebSite
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // button calls the same code as the scheduled task
+        protected void btnRunSomeWork_OnClick(object sender, EventArgs e)
+        {
+            SomeWork.TruncateEventLog();
+        }
+    }
+}
